e38a550658208d76282dd8cc7d78a76478e21cfa
ozepon <dandy.buzz@gmail.com>
2016-12-26 02:44:47 +0900

    develop 追加

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,2 +1,2 @@
-javascript:(function(){var time = new Date().getTime();var s = document.createElement("script");s.src = "https://cdn.rawgit.com/ozepon/cyocon_bookmarklet/master/js/main.js?v=" + time;s.charset="UTF-8";document.body.appendChild(s);})();
-https://cdn.rawgit.com/ozepon/cyocon_bookmarklet/master/js/main.js?v=
+javascript:(function(){var time = new Date().getTime();var s = document.createElement("script");s.src = "https://cdn.rawgit.com/ozepon/cyocon_bookmarklet/develop/js/main.js?v=" + time;s.charset="UTF-8";document.body.appendChild(s);})();
+https://cdn.rawgit.com/ozepon/cyocon_bookmarklet/develop/js/main.js?v=
