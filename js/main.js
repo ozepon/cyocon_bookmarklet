938e89c339e4ae42ecc60d06f7cb9200d5e560c2
ozepon <dandy.buzz@gmail.com>
2017-01-11 02:09:36 +0900

    add panty mode

--- js/main.js
+++ js/main.js
@@ -21,25 +21,48 @@ var video = video_box.children[0];
 video.style.width = "100%";
 video.style.height = "178%";
 
+
+// Panty Mode
+var is_panty = true;  
+
+var comment_body = $('.LyTop')
+comment_body.attr('style', 'position :relative;z-index:1;');
+
 // startボタン
 var start_button = document.createElement('div');
-start_button.style
+start_button.innerText = 'Panty Mode';
+start_button.style.background = 'rgb(255, 185, 185)';
+start_button.style.position = 'absolute';
+start_button.style.top = '53px';
+start_button.style.right = 0;
+start_button.style.zIndex = 100000;
+start_button.style.width = '10em';
+start_button.style.textAlign = 'center';
+start_button.style.height = '2em';
+start_button.style.lineHeight = '2em';
+start_button.style.borderRadius = '5px'
 
-$('.MdBtn01Like').click(function() {
-  setTimeout(function(){
-    $('.mdMN15Write').eq(0).click(function() {
-      // $(this).val( message); 
-      $(this).attr('value', message + message); 
-    });
-  }, 0);
-  console.log('foo');
-  var message = 'いい音';
-  $('input.mdMN15Write').val( message); 
-  $('.mdMN15Write').attr('value', message); 
-  $('.mdMN15Write').attr('value', message + message); 
+  
+if ($('.mdMN14Ttl').children()[0].innerText.match(/けんてぃ/) === null) {
+  comment_body.append(start_button);  
+}
 
-  $('.mdMN15Send').click();
-});
+start_button.onclick = function() {
+  is_panty = !is_panty;
+  if (is_panty) {
+    start_button.innerText = 'Yes Panty!'; 
+    start_button.style.background = 'rgb(255, 185, 185)';    
+  }else {
+    start_button.innerText = 'No Panty...'; 
+    start_button.style.background = 'rgb(40, 40, 160)';
+  }
+  var synthes = new SpeechSynthesisUtterance(start_button.innerText);
+  synthes.lang = "ja-JP";
+  synthes.pitch = 1.5;
+  synthes.rate = 0.6;
+  synthes.volume = 0.5;
+  speechSynthesis.speak(synthes);  
+};
 
 // 棒読みちゃん
 var tmp_comment = '';
@@ -47,7 +70,6 @@ var tmp_comment = '';
 // 置換する文字列
 var replase_map = {'甲虫装機':'インセクター',
                   '🌻':''};
-                  
 $($('.mdMN15Scroll')[0]).bind('DOMSubtreeModified', function(e) {
   var comment = e.target.lastElementChild.innerText;
   // 読み方を変更する
@@ -55,6 +77,13 @@ $($('.mdMN15Scroll')[0]).bind('DOMSubtreeModified', function(e) {
     console.log('key' + key);
     var reg = new RegExp(key, 'g');
     comment = comment.replace(reg,replase_map[key]);
+  }
+  if(is_panty) {
+    var key = 'けんてぃ';
+    var replace = 'ぱんてぃ';
+    console.log('key' + key);
+    var reg = new RegExp(key, 'g');
+    comment = comment.replace(reg,replace);
   }
 
   console.log(comment);
